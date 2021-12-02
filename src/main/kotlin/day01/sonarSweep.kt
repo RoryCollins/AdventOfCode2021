808e13239bf29f2f3430b2f0c983d2b13d590246
Rory Collins <rory.collins@hotmail.co.uk>
2021-12-02 10:51:28 +0000

    refactorings

--- src/main/kotlin/day01/sonarSweep.kt
+++ src/main/kotlin/day01/sonarSweep.kt
@@ -2,10 +2,11 @@ package day01
 
 import java.io.File
 
+val numbers = File("src/main/kotlin/day01/input.txt")
+    .readLines()
+    .map { it.toInt() }
+
 fun main(args: Array<String>) {
-    val numbers = File("src/main/kotlin/day01/input.txt")
-        .readLines()
-        .map { it.toInt() }
     println("Part One: ${numbers.windowed(2).count { it.last() > it.first() }}")
     println("Part Two: ${numbers.windowed(4).count { it.last() > it.first() }}")
 }
--- src/main/kotlin/day02/dive.kt
+++ src/main/kotlin/day02/dive.kt
@@ -2,24 +2,25 @@ package day02
 
 import java.io.File
 
-data class Point(val X:Int, val Y:Int, val Aim:Int)
-
 val instructions = File("src/main/kotlin/day02/input.txt")
     .readLines()
-    .map{it.split(" ")}
+    .map { it.split(" ") }
+    .map { it.first() to it.last().toInt() }
 
-fun parseInstruction(instruction:List<String>, current:Point): Point {
-    val direction = instruction.first()
-    val distance = instruction.last().toInt()
-    return when(direction){
-        "forward" -> Point(current.X + distance, current.Y+(current.Aim * distance), current.Aim)
-        "up" -> Point(current.X, current.Y, current.Aim-distance)
-        "down" -> Point(current.X, current.Y, current.Aim+distance)
+data class Point(val X: Int, val Y: Int, val Aim: Int)
+
+fun parseInstruction(instruction: Pair<String, Int>, current: Point): Point {
+    val (direction, distance) = instruction
+    return when (direction) {
+        "forward" -> Point(current.X + distance, current.Y + (current.Aim * distance), current.Aim)
+        "up" -> Point(current.X, current.Y, current.Aim - distance)
+        "down" -> Point(current.X, current.Y, current.Aim + distance)
         else -> throw IllegalArgumentException(direction)
     }
 }
 
-fun main(){
-    val destination = instructions.fold(Point(0, 0, 0)){ current, instruction -> parseInstruction(instruction, current)}
+fun main() {
+    val destination =
+        instructions.fold(Point(0, 0, 0)) { current, instruction -> parseInstruction(instruction, current) }
     println(destination.X * destination.Y)
 }
