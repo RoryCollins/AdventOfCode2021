fbd24b0ef6705ec9b71e7962db29b2cbb3454cf8
Rory Collins <rory.collins@hotmail.co.uk>
2021-12-03 10:26:43 +0000

    refactor

--- src/main/kotlin/day02/dive.kt
+++ src/main/kotlin/day02/dive.kt
@@ -7,20 +7,20 @@ val instructions = File("src/main/kotlin/day02/input.txt")
     .map { it.split(" ") }
     .map { it.first() to it.last().toInt() }
 
-data class Point(val X: Int, val Y: Int, val Aim: Int)
+data class State(val X: Int, val Y: Int, val Aim: Int)
 
-fun parseInstruction(instruction: Pair<String, Int>, current: Point): Point {
+fun parseInstruction(instruction: Pair<String, Int>, current: State): State {
     val (direction, distance) = instruction
     return when (direction) {
-        "forward" -> Point(current.X + distance, current.Y + (current.Aim * distance), current.Aim)
-        "up" -> Point(current.X, current.Y, current.Aim - distance)
-        "down" -> Point(current.X, current.Y, current.Aim + distance)
+        "forward" -> State(current.X + distance, current.Y + (current.Aim * distance), current.Aim)
+        "up" -> State(current.X, current.Y, current.Aim - distance)
+        "down" -> State(current.X, current.Y, current.Aim + distance)
         else -> throw IllegalArgumentException(direction)
     }
 }
 
 fun main() {
     val destination =
-        instructions.fold(Point(0, 0, 0)) { current, instruction -> parseInstruction(instruction, current) }
+        instructions.fold(State(0, 0, 0)) { current, instruction -> parseInstruction(instruction, current) }
     println(destination.X * destination.Y)
 }
