af1575d9cb222785d2c12880eb4edf16d1278b50
Rory Collins <rory.collins@hotmail.co.uk>
2021-12-09 16:43:11 +0000

    add day09

---
+++ src/main/kotlin/day09/smokeBasin.kt
@@ -0,0 +1,58 @@
+package day09
+
+import java.io.File
+
+data class Coordinate(val X: Int, val Y: Int) {
+    fun up(): Coordinate = Coordinate(X, Y - 1)
+    fun down(): Coordinate = Coordinate(X, Y + 1)
+    fun right(): Coordinate = Coordinate(X + 1, Y)
+    fun left(): Coordinate = Coordinate(X - 1, Y)
+}
+
+class Grid(private val rows: List<List<Int>>) {
+    private val lowPoints = rows.indices.flatMap { y ->
+        rows.first().indices
+            .filter { x -> getNeighbours(Coordinate(x, y)).all { rows[it.Y][it.X] > rows[y][x] } }
+            .map { x -> Pair(Coordinate(x, y), rows[y][x] + 1) }
+    }
+
+    val lowPointRisk: Int = lowPoints.sumOf { (_, risk) -> risk }
+
+    val basinSizesDescending: List<Int> = lowPoints
+        .map{(coordinate, _) -> getBasin(setOf(coordinate)).size}
+        .sortedDescending()
+
+    private fun getBasin(acc: Set<Coordinate>): Set<Coordinate> {
+        val next = acc + acc.flatMap { getNeighbours(it) }.filter { rows[it.Y][it.X] != 9 }
+        return if (next.size == acc.size) acc
+        else getBasin(next)
+    }
+
+    private fun getNeighbours(coordinate: Coordinate): List<Coordinate> {
+        return listOf(
+            coordinate.up(),
+            coordinate.down(),
+            coordinate.left(),
+            coordinate.right()
+        ).filter { !isOutOfBounds(it) }
+    }
+
+    private fun isOutOfBounds(coordinate: Coordinate): Boolean {
+        return coordinate.X < 0 || coordinate.Y < 0 || coordinate.X >= rows.first().size || coordinate.Y >= rows.size
+    }
+}
+
+
+fun main() {
+    val rows = File("src/main/kotlin/day09/input_test.txt")
+        .readLines()
+        .map { line -> line.map { Character.getNumericValue(it) } }
+
+    val grid = Grid(rows)
+
+    println("Part One: ${grid.lowPointRisk}")
+    println("Part Two: ${grid.basinSizesDescending
+            .take(3)
+            .fold(1) { a, b -> a * b }
+    }")
+}
