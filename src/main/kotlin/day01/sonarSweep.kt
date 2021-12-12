7bb50686be0e215866a6d703a0fe5d7a9436c262
Rory Collins <rory.collins@hotmail.co.uk>
2021-12-12 10:31:53 +0000

    remove unnecessary args

--- src/main/kotlin/day01/sonarSweep.kt
+++ src/main/kotlin/day01/sonarSweep.kt
@@ -6,7 +6,7 @@ val numbers = File("src/main/kotlin/day01/input.txt")
     .readLines()
     .map { it.toInt() }
 
-fun main(args: Array<String>) {
+fun main() {
     println("Part One: ${numbers.windowed(2).count { it.last() > it.first() }}")
     println("Part Two: ${numbers.windowed(4).count { it.last() > it.first() }}")
 }
