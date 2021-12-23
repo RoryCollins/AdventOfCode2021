21a0a205b7864e2f29d87f3898aec9e2425b0d02
Rory Collins <rory.collins@hotmail.co.uk>
2021-12-23 07:44:51 +0000

    finish day18

--- src/main/kotlin/day18/snailfish.kt
+++ src/main/kotlin/day18/snailfish.kt
@@ -2,7 +2,8 @@ package day18
 
 import shared.Solution
 import java.io.File
-import kotlin.reflect.typeOf
+import kotlin.math.ceil
+import kotlin.math.floor
 
 fun main() {
     val input = File("src/main/kotlin/day18/input.txt").readLines()
@@ -10,74 +11,93 @@ fun main() {
 }
 
 fun solve(input: List<String>): Solution<Int, Int> {
-    return Solution(0, 0)
+    val init = input.first()
+    val rest = input.drop(1)
+
+    val total = rest.fold(init){ acc, s -> acc.add(s) }
+
+    var greatestMagnitude = 0
+    for(line in input){
+        greatestMagnitude = maxOf(greatestMagnitude, input.filter{it != line}.maxOf{magnitude(line.add(it))})
+    }
+    return Solution(magnitude(total), greatestMagnitude)
+}
+
+fun String.add(other: String) : String{
+    val result = "[$this,$other]"
+    return reduce(result)
+}
+
+private fun reduce(number: String): String{
+    var current = ""
+    var reduced = number
+    while(current != reduced){
+        current = reduced
+        val result = explode(current, 0, 0, "")
+        reduced = result.content.replace("[]", "0")
+        if(result.exploded) continue
+        reduced = split(current, "")
+    }
+    return reduced
+}
+
+fun explode(number: String): String{
+    val explosionData = explode(number, 0, 0, "")
+    return explosionData.content.replace("[]", "0")
+}
+
+fun explode(number: String, nestLevel: Int, carriedRight: Int, prefix: String, exploding: Boolean = false): ExplosionData{
+    if(number.isEmpty()) return ExplosionData(false, 0, prefix)
+    if(number.first() == '[') return explode(number.drop(1), nestLevel+1, carriedRight, "$prefix[", exploding)
+    if(number.first() == ']') return explode(number.drop(1), nestLevel-1, carriedRight,"$prefix]", exploding)
+    if(number.first() == ',') return explode(number.drop(1), nestLevel, carriedRight, "$prefix,", exploding)
+    val (leftString, rest) = """^(\d*)(.*)""".toRegex().find(number)?.destructured ?: error("Oh no")
+    val left = leftString.toInt()
+    if(exploding){
+        return ExplosionData(true, 0, "$prefix${left+carriedRight}$rest")
+    }
+    if (nestLevel > 4){
+        val right = """^,(\d*).*""".toRegex().find(rest)?.groups?.get(1)?.value?.toInt() ?: error ("uh oh")
+        val number1 = number.drop(number.indexOf(']'))
+        val content = explode(number1, nestLevel, right, prefix, true)
+        return ExplosionData(true, left, content.content)
+    }
+    val content = explode(number.drop(leftString.length), nestLevel, carriedRight, "")
+    return ExplosionData(content.exploded, 0, "$prefix${left+content.left}${content.content}")
+}
+
+fun split(number:String): String{
+    return split(number, "")
+}
+fun split(number: String, result: String): String{
+    if(number.isEmpty()) return result
+    if("[,]".contains(number.first())) return split(number.drop(1), result + number.first())
+    val (numberString, rest) = """^(\d*)(.*)""".toRegex().find(number)?.destructured ?: error("oh no")
+    val subject = numberString.toInt()
+    if (subject > 9){
+        val left = floor(subject/2.0).toInt()
+        val right = ceil(subject/2.0).toInt()
+        return "$result[$left,$right]$rest"
+    }
+    return split(rest, result + numberString)
+}
+
+fun magnitude(number: String): Int{
+    return getMagnitude(number).first
+}
+
+private fun getMagnitude(number: String): Pair<Int, String>{
+    if(number.isEmpty()) return 0 to ""
+    if(number.first() == '[') {
+        val (left, rest) = getMagnitude(number.drop(1))
+        val (right, _rest) = getMagnitude(rest)
+        return (3*left) + (2*right) to _rest
+    }
+    if(number.first().isDigit()){
+        return Character.getNumericValue(number.first()) to number.drop(1)
+    }
+    return getMagnitude(number.drop(1))
 }
 
 
-abstract class SnailfishNumber() {
-    abstract fun magnitude(): Int
-    abstract fun nest(): SnailfishNumber
-    fun plus(other: SnailfishNumber): SnailfishNumber {
-        return SnailfishPair(this.nest(), other.nest())
-    }
-
-    abstract fun plus(carryOver: CarryOver): SnailfishNumber
-    abstract fun plus(number: Int): SnailfishNumber
-    abstract fun explode(): Pair<SnailfishPair, CarryOver>
-    abstract fun isPair(): Boolean
-}
-
-data class SnailfishLiteral(private value: Int) : SnailfishNumber() {
-    override fun magnitude(): Int = value
-    override fun nest(): SnailfishNumber = this
-    override fun plus(carryOver: CarryOver): SnailfishNumber {
-        return SnailfishLiteral(value + carryOver.left + carryOver.right)
-    }
-
-    override fun plus(number: Int): SnailfishNumber {
-        return SnailfishLiteral(value + number)
-    }
-
-    override fun explode(): Pair<SnailfishPair, CarryOver> = error("this shouldn't happen")
-    override fun isPair(): Boolean = false
-}
-
-data class CarryOver(val left:Int, val right:Int)
-
-data class SnailfishPair(
-    val left: SnailfishNumber,
-    val right: SnailfishNumber,
-    private val nestLevel: Int = 0
-) : SnailfishNumber() {
-    override fun magnitude(): Int {
-        return left.magnitude() * 3 + right.magnitude() * 2
-    }
-
-    override fun nest() = SnailfishPair(left.nest(), right.nest(), nestLevel + 1)
-    override fun plus(carryOver: CarryOver): SnailfishNumber {
-        return SnailfishPair(this.left.plus(carryOver.left), this.right.plus(carryOver.right))
-    }
-
-    override fun plus(number: Int): SnailfishNumber {
-        TODO("Not yet implemented")
-    }
-
-    override fun explode(): Pair<SnailfishPair, CarryOver>{
-        if(nestLevel==3){
-            if(left.isPair()){
-                val l = left as SnailfishPair
-                val carryOver = CarryOver(l.left.value, l.right)
-                right.plus(CarryOver(0, content.second.right))
-                return SnailfishPair(SnailfishLiteral(0), right, nestLevel) to CarryOver(0, content.second.right)
-            }
-            else if (right.isPair()) {
-                val content = right.explode()
-                left.plus(CarryOver(content.second.left, 0))
-                return SnailfishPair(SnailfishLiteral(0), right, nestLevel) to CarryOver(0, content.second.right)
-            }
-        }
-        return this to CarryOver(0,0)
-    }
-
-    override fun isPair(): Boolean = true
-}
+data class ExplosionData(val exploded: Boolean, val left: Int, val content: String)
--- src/test/kotlin/day18/SnailfishTest.kt
+++ src/test/kotlin/day18/SnailfishTest.kt
@@ -3,78 +3,104 @@ package day18
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import shared.Solution
 
 internal class SnailfishTest {
-    private val one = SnailfishLiteral(1)
-    private val two = SnailfishLiteral(2)
-    private val three = SnailfishLiteral(3)
-    private val four = SnailfishLiteral(4)
-    private val five = SnailfishLiteral(5)
-    private val six = SnailfishLiteral(6)
-    private val seven = SnailfishLiteral(7)
-    private val eight = SnailfishLiteral(8)
-    private val nine = SnailfishLiteral(9)
-    private val zero = SnailfishLiteral(0)
-    private val nineOne = SnailfishPair(SnailfishLiteral(9), SnailfishLiteral(1))
-    private val oneNine = SnailfishPair(SnailfishLiteral(1), SnailfishLiteral(9))
-
     @Test
-    fun simpleMagnitude() {
-        assertEquals(29, nineOne.magnitude())
-        assertEquals(21, oneNine.magnitude())
+    fun addSnailfishNumbers() {
+        val oneOne = "[1,1]"
+        val twoTwo = "[2,2]"
+        val expected = "[[1,1],[2,2]]"
+        assertEquals(expected, oneOne.add(twoTwo))
     }
 
     @Test
-    fun compoundMagnitude() {
-        val compound = SnailfishPair(nineOne, oneNine)
-        assertEquals(129, compound.magnitude())
-
-        val eightSeven = SnailfishPair(eight, seven)
-        val eightSix = SnailfishPair(eight, six)
-        val sixSix = SnailfishPair(six, six)
-        val sevenSeven = SnailfishPair(seven, seven)
-        val zeroSeven = SnailfishPair(zero, seven)
-        val magnitude = SnailfishPair(
-            SnailfishPair(
-                SnailfishPair(eightSeven, sevenSeven),
-                SnailfishPair(eightSix, sevenSeven)
-            ),
-            SnailfishPair(
-                SnailfishPair(zeroSeven, sixSix),
-                eightSeven
-            )
-        ).magnitude()
-        assertEquals(3488, magnitude)
+    fun explodeMiddleSnailfishNumber() {
+        val test = explode("[[[2,[[1,1],2]]]]")
+        val expected = "[[[3,[0,3]]]]"
+        assertEquals(expected, test)
     }
 
     @Test
-    fun simpleAddition() {
-        assertEquals(oneNine, one.plus(nine))
+    fun explodeLeftSnailFishNumber() {
+        val test = explode("[[[[[1,1],2]]]]")
+        val expected = "[[[[0,3]]]]"
+        assertEquals(expected, test)
     }
 
     @Test
-    fun additionWithExplosion() {
-        val actual = SnailfishPair(one, one)
-            .plus(SnailfishPair(two, two))
-            .plus(SnailfishPair(three, three))
-            .plus(SnailfishPair(four, four))
-            .plus(SnailfishPair(five, five))
+    fun explodeRightSnailfishNumber() {
+        val test = explode("[[[[2,[1,1]]]]]")
+        val expected = "[[[[3,0]]]]"
+        assertEquals(expected, test)
+    }
 
-        val expected = SnailfishPair(
-            SnailfishPair(
-                SnailfishPair(three, zero),
-                SnailfishPair(five, three)),
-            SnailfishPair(
-                SnailfishPair(four, four),
-                SnailfishPair(five, five)))
+    @Test
+    fun onlyExplodeSinglePairPerPass() {
+        val test = explode("[[[[[1,1],[2,2]]]]]")
+        val expected = "[[[[0,[3,2]]]]]"
+        assertEquals(expected, test)
+    }
 
+    @Test
+    fun explodeDoesNothingForSimpleNumber() {
+        val test = "[1,1]"
+        val expected = ExplosionData(false, 0, test)
+        assertEquals(expected, explode(test, 0, 0, ""))
+    }
+
+    @Test
+    fun explodeWhenAddNumbers() {
+        val actual = "[1,1]"
+            .add("[2,2]")
+            .add("[3,3]")
+            .add("[4,4]")
+            .add("[5,5]")
+
+        val expected = "[[[[3,0],[5,3]],[4,4]],[5,5]]"
         assertEquals(expected, actual)
     }
 
     @Test
-    fun explodeReturnsCorrectCarryOver(){
-        val actual = SnailfishPair(nineOne, one,3).explode()
-        val expected = SnailfishPair(zero, two, 3) to CarryOver(9, 0)
-        assertEquals(expected, actual)
+    fun splitNumber() {
+        val test = split("[11,1]")
+        val expected = "[[5,6],1]"
     }
+
+    @Test
+    fun complexAddition() {
+//        val actual = "[[[[4,3],4],4],[7,[[8,4],9]]]".add("[1,1]")
+//        val expected = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+//        assertEquals(expected, actual)
+
+        assertEquals("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]", "[[[[7,7],[7,7]],[[8,7],[8,7]]],[[[7,0],[7,7]],9]]".add("[[[[4,2],2],6],[8,7]]"))
+    }
+
+    @Test
+    fun getMagnitude() {
+        assertEquals(29, magnitude("[9,1]"))
+        assertEquals(129, magnitude("[[9,1],[1,9]]"))
+        assertEquals(3488, magnitude("[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"))
+    }
+
+    @Test
+    fun solve() {
+        val lines = listOf(
+            "[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]",
+            "[[[5,[2,8]],4],[5,[[9,9],0]]]",
+            "[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]",
+            "[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]",
+            "[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]",
+            "[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]",
+            "[[[[5,4],[7,7]],8],[[8,3],8]]",
+            "[[9,3],[[9,9],[6,[4,9]]]]",
+            "[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]",
+            "[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]"
+        )
+
+        val solution = solve(lines)
+        val expected = Solution(4140, 3993)
+        assertEquals(expected, solution)
+    }
+
 }
